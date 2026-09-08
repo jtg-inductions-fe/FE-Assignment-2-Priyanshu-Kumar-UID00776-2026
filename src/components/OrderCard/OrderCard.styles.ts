a43b62a09310aea8fc67f95485b3e0d36ad2bc8f
Priyanshu-workSpace <priyanshu.kumar@joshtechnologygroup.com>
2026-09-08 13:14:39 +0530

    [PK_A2_07]: Public cache updated

--- src/components/OrderCard/OrderCard.styles.ts
+++ src/components/OrderCard/OrderCard.styles.ts
@@ -3,7 +3,7 @@ import { Card, CardActionArea, styled } from '@mui/material';
 export const OrderStyledCard = styled(Card)({
     borderRadius: '20px',
     overflow: 'hidden',
-    boxShadow: '0 2px 15px rgba(0, 0, 0, 0.2)',
+    boxShadow: '0 2px 14px rgba(0, 0, 0, 0.2)',
 });
 
 export const OrderActionArea = styled(CardActionArea)(({ theme }) => ({
